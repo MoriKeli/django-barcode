aa1c27389aea9fc3ca6535ad16ed60128cf53f99
Mori Keli <kelimbth@gmail.com>
2022-12-05 13:25:32 +0300

    created a db table - products

--- sales/models.py
+++ sales/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
 
-class Product(models.Model):
+class Products(models.Model):
+    id = models.CharField(max_length=12, primary_key=True, unique=True, editable=False)
+    product_id = models.CharField(max_length=6, blank=False)
+    country_id = models.CharField(max_length=1, null=False)
+    manufacturer_id = models.CharField(max_length=7, blank=False)
+
     name = models.CharField(max_length=50, blank=False)
+    barcode = models.ImageField(upload_to='barcodes/', blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Products'
     
+    
+    def __str__(self):
+        return f'{self.name}'
