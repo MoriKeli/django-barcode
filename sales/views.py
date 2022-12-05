551af52ee604f158e910965bfdc0f190bdd6ea3a
Mori Keli <kelimbth@gmail.com>
2022-12-05 14:32:25 +0300

    added homepage view

--- sales/views.py
+++ sales/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Products
+from .forms import NewProductForm
 
-# Create your views here.
+
+def homepage_view(request):
+    form = NewProductForm()
+    products = Products.objects.all()
+
+    if request.method == 'POST':
+        form = NewProductForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'New product saved successfully!')
+            return redirect('homepage')
+
+    context = {'form': form, 'products': products}
+    return render(request, 'sales/homepage.html', context)
