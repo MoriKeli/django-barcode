8e3f4d0df768cc2d77cdbadf74496ec06bc46396
Mori Keli <kelimbth@gmail.com>
2022-12-05 13:56:02 +0300

    updated Admin Panel UI

--- sales/admin.py
+++ sales/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Products
 
-admin.site.register(Products)
+@admin.register(Products)
+class ProductsTable(admin.ModelAdmin):
+    list_display = ['name', 'barcode']
+    ordering = ['name']
