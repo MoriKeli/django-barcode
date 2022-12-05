3d5fdb5951eb9b1a02a26208459da23f34ed6fc2
Mori Keli <kelimbth@gmail.com>
2022-12-05 14:42:17 +0300

    configured static files folder path

--- src/settings.py
+++ src/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -105,6 +106,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'assets/css'),
+]
+
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR/'media/'
